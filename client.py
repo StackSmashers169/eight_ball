2ccc7b388add35b1819d5da03ed10e71839b24b7
VicJo <VicJohnathanWilson@outlook.com>
2023-10-16 15:31:55 +0800

    Got eight ball working, also managed to get the non-question easter egg working as well.

--- client.py
+++ client.py
@@ -4,10 +4,20 @@ from server import ADDRESS
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(ADDRESS)
 
-message = input()
+# prompts client to send message.
+welcome_message = client_socket.recv(1024).decode('utf-8')
+print(welcome_message)
+message = input(" -> ")
 
-while message.lower() in ["quit", "exit"]:
-    print("Exit request sent to server, closing client")
-    break
 
+while message.lower() not in ["quit", "exit"]:
+    client_socket.send(message.encode('utf-8'))
+    data = client_socket.recv(1024).decode('utf-8')
+    print(data)
+    print()  # print empty line for easier reading
+
+    print(welcome_message)
+    message = input(" -> ")
+
+print("Exit request sent to server, closing client")
 client_socket.close()
--- server.py
+++ server.py
@@ -1,27 +1,40 @@
 import socket
-from random import choice
+import random
 
 ANSWERS = [
-    'It is certain'
-    'As I see it, yes'
-    'Reply hazy, try again'
-    'not counting on it'
-    'It is decidedly so'
-    'Most likely'
-    'Ask again later'
-    'My reply is no'
-    'Without a doubt'
-    'Outlook good'
-    'Better not tell you now'
-    'My sources say no'
-    'Definitely'
-    'Yes'
-    'Cannot predict now'
-    'Outlook is not good'
-    'You can rely on it'
-    'Signs say yes'
-    'Concentrate and ask again'
-    'Very doubtful'
+    'It is certain',
+    'As I see it, yes',
+    'not counting on it',
+    'It is decidedly so',
+    'Most likely',
+    'I think it will happen',
+    'My reply is no',
+    'Without a doubt',
+    'Outlook good',
+    'My sources say no',
+    'Definitely',
+    'Yes',
+    'Cannot predict now',
+    'Outlook is not good',
+    'You can rely on it',
+    'Signs say yes',
+    'Very doubtful',
+    'This is the correct forecast',
+    'Absolutely',
+    'Absolutely Not',
+    'Hah! Good one buddy',
+]
+
+NON_QUESTIONS = [
+    'Concentrate and ask again',
+    'Could you ask that again?',
+    'This is not a question, so I cannot give a response',
+    'No question mark found.. DOES NOT COMPUTE',
+    'I can ask the questions instead if your going to muck around',
+    'A computer cannot turn a non question into a question',
+    'Have you tried putting a ? on the end?',
+    'If you keep this up I will type exit or quit for you',
+    'Reply hazy, try again',
 ]
 
 ADDRESS = 'localhost', 1234
@@ -33,13 +46,24 @@ if __name__ == '__main__':
     server_socket.bind(ADDRESS)
     server_socket.listen(5)
 
-    while True:
-        client_socket, client_address = server_socket.accept()
+    client_socket, client_address = server_socket.accept()
 
-        initial_message = "Write a message to the eight ball"
-        client_socket.send(initial_message.encode())
+    initial_message = "Write a message to the eight ball"
+    client_socket.send(initial_message.encode())
 
-        # send and recieve responses from client
-        data = client_socket.recv(1024).decode('utf-8')
-        if not data:
-            break
+    with client_socket:
+        print(f"Connection at {ADDRESS} established")
+        while True:
+            # send and recieve responses from client
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+
+            if '?' in data:
+                message = random.choice(ANSWERS)
+                print(message)
+                client_socket.send(message.encode('utf-8'))
+            else:
+                message = random.choice(NON_QUESTIONS)
+                client_socket.send(message.encode('utf-8'))
+
